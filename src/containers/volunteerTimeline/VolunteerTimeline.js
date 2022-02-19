2008c3df216a0e33403a466226acf7c3f7bfdb8e
ammar <ammarraneez@gmail.com>
2022-02-19 21:37:32 +0530

    feat: volunteer experience

--- src/containers/volunteerTimeline/VolunteerTimeline.js
+++ src/containers/volunteerTimeline/VolunteerTimeline.js
@@ -27,6 +27,11 @@ function VolunteerTimeline({ work }) {
           >
             {work.company}
           </h4>
+          <img
+            src={require(`../../assests/images/${work.logo_path}`)}
+            style={{ width: '70px', objectFit: 'contain', marginTop: '10px' }}
+            alt={work.company}
+          />
           <p>
             {work.description}
           </p>
