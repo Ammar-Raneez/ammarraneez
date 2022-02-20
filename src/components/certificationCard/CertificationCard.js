70ddd159844c54888308867cfa8567efd617d84e
ammar <ammarraneez@gmail.com>
2022-02-20 17:30:17 +0530

    feat: animated education

--- src/components/certificationCard/CertificationCard.js
+++ src/components/certificationCard/CertificationCard.js
@@ -9,44 +9,46 @@ class CertificationCard extends Component {
     return (
       <Fade bottom duration={2000} distance="20px">
         <div className="cert-card">
-          <div className="content">
-            <a
-              href={certificate.certificate_link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                className="content-overlay"
-                style={{ backgroundColor: certificate.overlay_bg_color }}
+          <div className="cert-animated-container">
+            <div className="content">
+              <a
+                href={certificate.certificate_link}
+                target="_blank"
+                rel="noopener noreferrer"
               >
-              </div>
-              <div
-                className="cert-header"
-                style={{ backgroundColor: certificate.color_code }}
+                <div
+                  className="content-overlay"
+                  style={{ backgroundColor: certificate.overlay_bg_color }}
+                >
+                </div>
+                <div
+                  className="cert-header"
+                  style={{ backgroundColor: certificate.color_code }}
+                >
+                  <img
+                    className="logo_img"
+                    src={require(`../../assests/images/${certificate.logo_path}`)}
+                    alt={certificate.alt_name}
+                  />
+                </div>
+                <div className="content-details fadeIn-top">
+                  <h3 className="content-title" style={{ color: theme.body }}>
+                    {certificate.title}
+                  </h3>
+                </div>
+              </a>
+            </div>
+            <div className="cert-body">
+              <h2 className="cert-body-title" style={{ color: theme.text }}>
+                {certificate.title}
+              </h2>
+              <h3
+                className="cert-body-subtitle"
+                style={{ color: theme.secondaryText }}
               >
-                <img
-                  className="logo_img"
-                  src={require(`../../assests/images/${certificate.logo_path}`)}
-                  alt={certificate.alt_name}
-                />
-              </div>
-              <div className="content-details fadeIn-top">
-                <h3 className="content-title" style={{ color: theme.body }}>
-                  {certificate.title}
-                </h3>
-              </div>
-            </a>
-          </div>
-          <div className="cert-body">
-            <h2 className="cert-body-title" style={{ color: theme.text }}>
-              {certificate.title}
-            </h2>
-            <h3
-              className="cert-body-subtitle"
-              style={{ color: theme.secondaryText }}
-            >
-              {certificate.subtitle}
-            </h3>
+                {certificate.subtitle}
+              </h3>
+            </div>
           </div>
         </div>
       </Fade>
